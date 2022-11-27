572f96db39322b694199a066a8dc309d9a52f1e1
Juan Rosario <jrosario19@hotmail.com>
2022-11-27 17:05:35 -0400

    Add forgotPassword feature

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -4,63 +4,48 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { SnackbarService } from 'src/services/snackbar.service';
 import { UserService } from 'src/services/user.service';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
+import {NgxUiLoaderService} from 'ngx-ui-loader'
 import { GlobalConstants } from '../shared/global-constants';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss'],
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss']
 })
-export class SignupComponent implements OnInit {
-  signupForm: any = FormGroup;
-  responseMessage: any;
+export class ForgotPasswordComponent implements OnInit {
+  forgotPasswordForm:any=FormGroup;
+  responseMessage:any;
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private userService: UserService,
-    private snackbarService: SnackbarService,
-    private dialogRef: MatDialogRef<SignupComponent>,
-    private ngxService: NgxUiLoaderService
+    private snackbarService:SnackbarService,
+    private dialogRef:MatDialogRef<ForgotPasswordComponent>,
+    private ngxService:NgxUiLoaderService
   ) {}
 
   ngOnInit(): void {
-    this.signupForm = this.formBuilder.group({
-      name: [
-        null,
-        [Validators.required, Validators.pattern(GlobalConstants.nameRegex)],
-      ],
+    this.forgotPasswordForm = this.formBuilder.group({
       email: [
         null,
         [Validators.required, Validators.pattern(GlobalConstants.emailRegex)],
       ],
-      contactNumber: [
-        null,
-        [
-          Validators.required,
-          Validators.pattern(GlobalConstants.contactNumberRegex),
-        ],
-      ],
-      password: [null, [Validators.required]],
     });
   }
 
   handleSubmit() {
     this.ngxService.start();
-    var formData = this.signupForm.value;
+    var formData = this.forgotPasswordForm.value;
     var data = {
-      name: formData.name,
       email: formData.email,
-      contactNumber: formData.contactNumber,
-      password: formData.password,
     };
-    this.userService.signup(data).subscribe(
+    this.userService.forgotPassword(data).subscribe(
       (response: any) => {
         this.ngxService.stop();
         this.dialogRef.close();
         this.responseMessage = response?.message;
         this.snackbarService.openSnackBar(this.responseMessage, '');
-        this.router.navigate(['/']);
+        //this.router.navigate(['/']);
       },
       (error) => {
         this.ngxService.stop();
@@ -76,4 +61,5 @@ export class SignupComponent implements OnInit {
       }
     );
   }
+
 }
