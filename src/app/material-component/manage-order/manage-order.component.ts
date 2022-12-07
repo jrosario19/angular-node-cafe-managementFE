5ade7c0cb312974a9a51935bd4892dfcf0c57225
Juan Rosario <jrosario19@hotmail.com>
2022-12-07 19:52:24 -0400

    Update manageOrder component

--- src/app/material-component/manage-order/manage-order.component.ts
+++ src/app/material-component/manage-order/manage-order.component.ts
@@ -1,3 +1,4 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
@@ -34,6 +35,7 @@ export class ManageOrderComponent implements OnInit {
 
   ngOnInit(): void {
     this.ngxService.start();
+    this.getCategories();
     this.manageOrderForm=this.formBuilder.group({
       name:[null,[Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
       email:[null,[Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
@@ -46,6 +48,87 @@ export class ManageOrderComponent implements OnInit {
       total:[0,[Validators.required]],
 
     })
+  }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe((response:any)=>{
+      this.ngxService.stop();
+      this.categories=response;
+    }, (error:any)=>{
+      this.ngxService.stop();
+      if(error.error?.message){
+        this.responseMessage=error.error?.message;
+
+      }else{
+        this.responseMessage=GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error)
+    })
+  }
+
+  getProductsByCategory(value:any){
+    this.productService.getProductsByCategory(value.id).subscribe((response:any)=>{
+      this.products=response;
+      this.manageOrderForm.controls['price'].setValue('');
+      this.manageOrderForm.controls['quantity'].setValue('');
+      this.manageOrderForm.controls['total'].setValue(0);
+    },(error:any)=>{
+      this.ngxService.stop();
+      if(error.error?.message){
+        this.responseMessage=error.error?.message;
+
+      }else{
+        this.responseMessage=GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error)
+    })
+  }
+
+  getProductDetails(value:any){
+    this.productService.getById(value.id).subscribe((response:any)=>{
+      this.price=response.price;
+      this.manageOrderForm.controls['price'].setValue(response.price);
+      this.manageOrderForm.controls['quantity'].setValue('1');
+      this.manageOrderForm.controls['total'].setValue(this.price*1);
+    }, (error:any)=>{
+      this.ngxService.stop();
+      if(error.error?.message){
+        this.responseMessage=error.error?.message;
+
+      }else{
+        this.responseMessage=GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error)
+    })
+  }
+
+  setQuantity(value:any){
+    var temp = this.manageOrderForm.controls['quantity'].value;
+    if(temp>0){
+      this.manageOrderForm.controls['total'].setValue(this.manageOrderForm.controls['quantity'].value * this.manageOrderForm.controls['price'].value)
+    }else if(temp!=''){
+      this.manageOrderForm.controls['quantity'].setValue('1');
+      this.manageOrderForm.controls['total'].setValue(this.manageOrderForm.controls['quantity'].value * this.manageOrderForm.controls['price'].value)
+    }
+  }
+
+  validateProductAdd(){
+    if(this.manageOrderForm.controls['total'].value===0 || this.manageOrderForm.controls['total'].value===null || this.manageOrderForm.controls['quantity'].value <=0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  validateSubmit(){
+    if(this.totalAmount===0 || this.manageOrderForm.controls['name'].value===null || 
+    this.manageOrderForm.controls['email'].value===null || this.manageOrderForm.controls['contactNumber'].value===null ||
+     this.manageOrderForm.controls['paymentMethod'].value===null || !(this.manageOrderForm.controls['contactNumber'].valid) || 
+     !(this.manageOrderForm.controls['email'].valid)){
+      return true;
+    }else{
+      return false;
+    }
   }
 
 }
